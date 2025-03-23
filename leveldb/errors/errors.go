694a4de6750c4dffe275c4aeb30114a382baf3d2
seph <seph@directionless.org>
2025-03-23 17:16:17 -0400

    wip

--- leveldb/errors/errors.go
+++ leveldb/errors/errors.go
@@ -3,7 +3,7 @@
 //
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+//
 // Package errors provides common error types used throughout leveldb.
 package errors
 
